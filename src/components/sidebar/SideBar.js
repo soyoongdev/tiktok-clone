b587a005f28f5b2c7e6e755b4a6d7f6829012d94
soyoong <huuhau.hh47@gmail.com>
2023-03-12 22:15:44 +0700

    Create global style

---
+++ src/components/sidebar/SideBar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './SideBar.module.css'
+
+function SideBar() {
+  return <div>SideBar</div>
+}
+
+export default SideBar
---
+++ src/styles/GlobalStyle/GlobalStyle.js
@@ -0,0 +1,8 @@
+import './GlobalCss.css'
+
+// Global css
+function GlobalStyle({ children }) {
+  return children
+}
+
+export default GlobalStyle
