baceddcc795832f52cf60b3c3ff82ad64b2d8c26
soyoong <huuhau.hh47@gmail.com>
2023-03-12 15:40:20 +0700

    use hook useImperativeHandle with video example

---
+++ src/pages/video/Video.js
@@ -0,0 +1,18 @@
+import { useRef, useImperativeHandle, forwardRef } from 'react'
+
+function Video(props, ref) {
+  const videoRef = useRef()
+
+  useImperativeHandle(ref, () => ({
+    playAction() {
+      videoRef.current.play()
+    },
+    pauseAction() {
+      videoRef.current.pause()
+    }
+  }))
+
+  return <video muted={false} ref={videoRef} src={props.src} width={450} />
+}
+
+export default forwardRef(Video) // forwardRef (HOC) sẽ trả về một props có tên là ref
